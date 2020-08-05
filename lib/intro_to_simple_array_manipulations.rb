696bfc6c353044038dbff997996af73f991104b0
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-05 17:20:35 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def push_method(array, string)
+  array.push(string)  
+end
+
+def unshift_method(array, string)
+  array.unshift(string)
+end
+
+def pop_method(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
